c3e7fe63918d19d00a50ec433b50d7fd8562b1cd
ksmushkov-cmd <ksmushkov@bk.ru>
2026-03-21 13:06:09 +0700

    Полностью передалнный код

---
+++ ConsoleApp1/ConsoleApp1/Memento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TextEditorApp {
+  public class TextMemento {
+    public string Content { get; private set; }
+    public DateTime Timestamp { get; private set; }
+
+    public TextMemento(string content) {
+      Content = content;
+      Timestamp = DateTime.Now;
+    }
+
+    public void Print() {
+      string preview = Content.Length > 30 ? Content.Substring(0, 30) + "..." : Content;
+      Console.WriteLine($"[{Timestamp:HH:mm:ss}] {preview}");
+    }
+  }
+}
